88c180b63f827603c6e15b07c3e059638cd6944a
Huajie Yin <63864156+y234xt@users.noreply.github.com>
2025-03-24 11:43:38 +0000

    return message changed

--- chapter6/main.py
+++ chapter6/main.py
@@ -51,7 +51,7 @@ def get_db():
     tags=["analytics"],
 )
 async def root():
-    return {"message": "API health check successful"}
+    return {"message": "This is an API health check: status successful"}
 
 
 @app.get(
